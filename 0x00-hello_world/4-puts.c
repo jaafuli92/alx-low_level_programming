1cd889a22f3579e0716c95e25e51540b8c5c989b
jaafuli1992 <jaafuli1992@gmail.com>
2023-03-16 16:15:03 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /*
  *
  * Puts program
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
