28b9bac78ced9281b66657866aa3853920cdef1d
jaafuli1992 <jaafuli1992@gmail.com>
2023-03-17 16:54:27 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: '0x01. C - Variables, if, else, while'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+	char c;
+
+	a = 0;
+	c = 'a';
+	while (a < 10)
+	{
+		putchar(a);
+		a++;
+	}
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
