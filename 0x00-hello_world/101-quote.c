52c58667329f4049395a9bd3ad42892df30da59e
jaafuli1992 <jaafuli1992@gmail.com>
2023-03-16 17:00:30 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints and that piece of art is useful - Dora Korpar
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
