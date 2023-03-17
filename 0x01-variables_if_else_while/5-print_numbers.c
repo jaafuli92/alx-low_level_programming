5b447d5dfcb7a28763fcd5ba877ec083dcbbc3c4
jaafuli1992 <jaafuli1992@gmail.com>
2023-03-17 15:38:26 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: '0x01. C - Variables, if, else, while'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c[9];
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(c[i]);
+	}
+	putchar('\n');
+	return (0);
+}
