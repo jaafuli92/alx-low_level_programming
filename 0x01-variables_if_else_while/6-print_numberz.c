ba197ca50270c574e7b84baf1774bf83a6b76069
jaafuli1992 <jaafuli1992@gmail.com>
2023-03-17 16:04:31 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: '0x01. C - Variables, if, else, while'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i=0; i<10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return(0);
+}
